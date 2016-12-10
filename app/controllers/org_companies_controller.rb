996b25239d87eba527a16b61b44c964d0edfba98
KING JP FLEURY <kingjpstar500@gmail.com>
2016-12-10 16:33:02 -0400

    a portial file od company field, controller and orgcompany new file and application that yml file has added in the config directory

---
+++ app/controllers/org_companies_controller.rb
@@ -0,0 +1,19 @@
+class OrgCompaniesController < ApplicationController
+    before_action :signed_in_user, only: [:new]
+
+    def new
+        signed_in_user # Be sure the user is signed in before he can create a company
+        @company = OrgCompany.new # Make new company object
+        @contactInfo = OrgContact.new.attributes # Make new contact object w/ empty attributes
+        @compnay.org_contacts.build(@contactInfo)
+    end
+
+    private
+        def signed_in_user
+            unless signed_in?
+                store_location
+                redirect_to signin_url, flash: {warning: "Please sign in."}
+            end
+        end
+
+end
